dc21dfe3420a39b1bea46672d489368e57ce289e
Joshua Howland <development@jkhowland.com>
2014-06-11 15:10:37 -0600

    Initial Commit

---
+++ Tic-Tac-Collection/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  Tic-Tac-Collection
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tic-Tac-Collection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tic-Tac-Collection
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
