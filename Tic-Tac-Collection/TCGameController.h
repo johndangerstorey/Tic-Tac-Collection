bd25af64d1798fdbe3f28becd73febfcdf790f45
John D. Storey <johndangerstorey@gmail.com>
2014-06-12 17:57:31 -0600

    Adds gameController, but not working

---
+++ Tic-Tac-Collection/TCGameController.h
@@ -0,0 +1,13 @@
+//
+//  TCGameController.h
+//  Tic-Tac-Collection
+//
+//  Created by John D. Storey on 6/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCGameController : NSObject
+
+@end
---
+++ Tic-Tac-Collection/TCGameController.m
@@ -0,0 +1,13 @@
+//
+//  TCGameController.m
+//  Tic-Tac-Collection
+//
+//  Created by John D. Storey on 6/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "TCGameController.h"
+
+@implementation TCGameController
+
+@end
